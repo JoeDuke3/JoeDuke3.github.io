114c62d375b47ee872a350cb1c4f8fe4a2b93748
JD <joeduke144@gmail.com>
2021-01-07 23:17:37 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// OPEN AND CLOSE THE CONTACT FORM
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// DISPLAY FIRST SLIDESHOW IMAGE UPON PAGE LOAD
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// CHANGE THE SLIDE WHEN AN ARROW IS CLICKED
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// CHANGE THE SLIDE WHEN A DOT IS CLICKED
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
